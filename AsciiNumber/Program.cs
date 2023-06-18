7d3348941f23f61e83637a5f43fa36a17dd6ba2a
omerfdev <omerfdev@gmail.com>
2023-06-18 22:06:11 +0300

    Commit

--- AsciiNumber/Program.cs
+++ AsciiNumber/Program.cs
@@ -1,10 +1,29 @@
 ﻿
-Console.WriteLine("Hello,Ascii World!");
-int number=int.Parse(Console.ReadLine());
-string binarycode = "";
-while (number>0)
+
+namespace AsciiNumber
 {
-    binarycode = (number % 2) + binarycode;
-    number=number/2;
+    public class Program
+    {
+        public delegate void asciiCodeDelegate();
+        public static asciiCodeDelegate AsciiCodedelegate = AsciiCode;
+        static void AsciiCode()
+        {
+            int number = int.Parse(Console.ReadLine());
+            string binaryCode = "";
+            while (number > 0)
+            {
+                binaryCode = (number % 2) + binaryCode;
+                number = number / 2;
+            }
+            Console.Write(binaryCode);
+
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello,Ascii World!");
+            AsciiCodedelegate();
+            Console.ReadLine();
+        }
+    }
 }
-Console.Write(binarycode);
+   
